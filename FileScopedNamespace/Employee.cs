9cf4dc24ee2a7f5af5334067cdef5199a91162b0
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-06 15:53:22 +0800

    Add project files.

---
+++ FileScopedNamespace/Employee.cs
@@ -0,0 +1,7 @@
+﻿namespace FileScopedNamespace;
+public class Employee
+{
+    public int ID { get; set; } = 0;
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
---
+++ FileScopedNamespace/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileScopedNamespace;
+internal class Program
+{
+    /// <summary>
+    /// Replace nested namespace and class
+    /// </summary>
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee();
+        employee.ID = 1001;
+        employee.Name = "Greg Esguerra";
+        employee.Address = "Cebu Philippines";
+        Console.WriteLine($"{employee.ID}, {employee.Name}, {employee.Address}");
+        Console.ReadLine();
+    }
+}
